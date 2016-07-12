9b65c6ce8f4a25ad25f4eff8e36303c4d1cadea5
Vinícius Borriello <vinicius.bo@gmail.com>
2016-07-12 20:59:17 -0300

    Implement text preprocessing

--- svm.py
+++ svm.py
@@ -4,11 +4,39 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfTransformer, HashingVectorizer
 from sklearn import svm
 from sklearn.metrics import classification_report
+from unidecode import unidecode
+import re
 
 def extract_features(docs):
+  docs = map(preprocess_text, docs)
   hashvector, vectorizer = hashing_vectorizer(docs)
   tfidf = get_tfidf(hashvector)
   return tfidf, vectorizer
+
+def preprocess_text(text):
+  text = text.lower()
+  text = unidecode(text)
+  text = remove_rt(text)
+  text = remove_twitter_user_mentions(text)
+  text = remove_hashtags(text)
+  text = remove_links(text)
+  # text = remove_numbers(text)
+  return text
+
+def remove_rt(text):
+  return re.sub('RT ', '', text)
+
+def remove_twitter_user_mentions(text):
+  return re.sub(r'(?:@[\w_]+)', '', text)
+
+def remove_hashtags(text):
+  return re.sub(r'(?:\#+[\w_]+[\w\'_\-]*[\w_]+)', '', text)
+
+def remove_links(text):
+  return re.sub(r'http\S+', '', text)
+
+def remove_numbers(text):
+  return re.sub(r'?:(?:\d+,?)+(?:\.?\d+)?)', '', text)
 
 def hashing_vectorizer(corpus):
   vectorizer = HashingVectorizer(stop_words=nltk.corpus.stopwords.words('portuguese'), strip_accents='ascii')
@@ -36,7 +64,7 @@ def build_report(clf, vectorizer, test_data):
 if __name__ == '__main__':
   # numpy.set_printoptions(threshold='nan')
   print 'Loading data...'
-  data = pd.read_csv('train_data.csv').as_matrix()
+  data = pd.read_csv('train_data.csv', encoding='utf8').as_matrix()
   print 'Loaded {} data'.format(len(data))
   print 'Finished data loading'
 
@@ -45,6 +73,6 @@ if __name__ == '__main__':
   print 'SVM trained'
 
   print 'Building report...'
-  test_data = pd.read_csv('test_data.csv').as_matrix()
+  test_data = pd.read_csv('test_data.csv', encoding='utf8').as_matrix()
   report = build_report(clf, vectorizer, test_data)
   print report
