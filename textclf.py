a39cb609118739aaaf5157ea99e6df335e118114
Vinícius Borriello <vinicius.bo@gmail.com>
2016-07-13 00:20:11 -0300

    Change Celery broker to RabbitMQ and other enhancements

--- textclf.py
+++ textclf.py
@@ -1,13 +1,28 @@
 import pandas as pd
 from celery import Celery
-from svm import train_svm, get_tfidf
+from svm import train_svm, tfidf_vectorizer
 
-app = Celery('textclf', backend='mongodb://127.0.0.1:27017/textclf_backend', broker='redis://127.0.0.1:6379/10')
-data = pd.read_csv('data.csv').as_matrix()
-clf, vectorizer = train_svm(data)
+celery = Celery('textclf', backend='amqp', broker='amqp://guest@localhost//')
+celery.conf.update(
+  CELERY_TASK_SERIALIZER='json',
+  CELERY_RESULT_SERIALIZER='json'
+)
+data = pd.read_csv('data.csv', encoding='utf8').as_matrix()
+clf = train_svm(data)
 
-@app.task
+@celery.task
 def classificate(tweet):
-  tfidf = get_tfidf(vectorizer.transform(tweet))
-  pred = clf.predict(tfidf)[0]
-  return pred
+  tfidf = tfidf_vectorizer.transform(tweet)
+  probabilities = clf.predict_proba(tfidf)[0]
+  pred = pick_winner_label(probabilities, threshold=0.5)
+  return list(probabilities), pred
+
+def pick_winner_label(probabilities, threshold):
+  winner = (-1, .0)
+  for (index, prob) in enumerate(probabilities):
+    if prob > threshold and prob > winner[1]:
+      winner = (index, prob)
+  return winner
+
+if __name__ == "__main__":
+  celery.start()
