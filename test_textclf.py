4f41d86b847646145cee0c3d92a659697a7d0a5f
Vinícius Borriello <vinicius.bo@gmail.com>
2016-07-16 15:00:13 -0300

    Fix test

--- test_textclf.py
+++ test_textclf.py
@@ -5,7 +5,7 @@ class TestTextClf(unittest.TestCase):
   def test_classificate_task(self):
     res = classificate.apply_async(args=['acho que estou gripado'])
     pred = res.wait(timeout=None, propagate=True, interval=0.5)
-    self.assertEqual(pred, 4)
+    self.assertEqual(pred[0][0], 4)
 
 if __name__ == '__main__':
     unittest.main()
