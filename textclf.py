79f7b6d7a11b2faca1622e50dc628d0e4d96cce9
Vinícius Borriello <vinicius.bo@gmail.com>
2016-07-16 15:00:36 -0300

    Refactor train out of textclf

--- textclf.py
+++ textclf.py
@@ -1,21 +1,18 @@
 import pandas as pd
 from celery import Celery
-from svm import train_svm, tfidf_vectorizer
+from svm import clf, tfidf_vectorizer
 
 celery = Celery('textclf', backend='amqp', broker='amqp://guest@localhost//')
 celery.conf.update(
   CELERY_TASK_SERIALIZER='json',
   CELERY_RESULT_SERIALIZER='json'
 )
-data = pd.read_csv('data.csv', encoding='utf8').as_matrix()
-clf = train_svm(data)
-
 @celery.task
 def classificate(tweet):
-  tfidf = tfidf_vectorizer.transform(tweet)
+  tfidf = tfidf_vectorizer.transform([tweet])
   probabilities = clf.predict_proba(tfidf)[0]
   pred = pick_winner_label(probabilities, threshold=0.5)
-  return list(probabilities), pred
+  return pred, list(probabilities)
 
 def pick_winner_label(probabilities, threshold):
   winner = (-1, .0)
